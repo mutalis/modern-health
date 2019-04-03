8b126755b7d6b70f58cb333462eb186e5f0c634a
rasg <rodolfo@mutalis.com>
2019-04-02 19:04:21 -0600

    Adds useContext to share the courses Info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { courses as coursesInfo } from './courses'
+
 const slugify = require('slugify')
 
-const courses = []
+const SectionsContext = React.createContext()
+
 const App = () => {
   const [courses, setCourses] = useState(coursesInfo)
 
@@ -19,10 +21,13 @@ const App = () => {
     ))
   }
   const programRoutes = () => {
-    return courses.map(({ name }) => (
-      <Route key={slugify(name)} path={`/${slugify(name)}`} component={Sections} />
+    return courses.map(course => (
+      <SectionsContext.Provider key={slugify(course.name)} value={course.sections}>
+        <Route path={`/${slugify(course.name)}`} component={Sections} />
+      </SectionsContext.Provider>
     ))
   }
+
   return (
     <Router>
       <div>
@@ -37,20 +42,23 @@ const App = () => {
 }
 
 const Sections = ({ match }) => {
+
+  const sections = useContext(SectionsContext)
+
   const sectionLinks = () => {
-    return courses[0].sections.map(({ name, order }) => (
-      <li>
-        <Link key={order} to={`${match.url}/${slugify(name)}`} >{name}</Link>
+    return sections.map(({ name, order }) => (
+      <li key={order}>
+        <Link to={`${match.url}/${slugify(name)}`} >{name}</Link>
       </li>
     ))
   }
+
   return (
     <div>
       <h2>Sections</h2>
       <ul>
         {sectionLinks()}
       </ul>
-
       <Route path={`${match.path}/:sectionId`} component={Section} />
       <Route
         exact
@@ -62,22 +70,26 @@ const Sections = ({ match }) => {
 }
 
 const Section = ({ match }) => {
+
+  const sections = useContext(SectionsContext)
+  const activities = sections.filter( section => slugify(section.name) === match.params.sectionId)[0].activities
+
   const sectionInfo = () => {
-    return courses[0].sections[0].activities.map(({ type, content }) => (
-      <li>
+    return activities.map(({ type, content }, index) => (
+      <li key={index}>
        {type}: {content}
       </li>
     ))
   }
+
   return (
     <div>
-      {console.log('AAA:',match)}
       <h3>{match.params.sectionId}</h3>
       <ul>
         {sectionInfo()}
       </ul>
     </div>
-  );
+  )
 }
 
 export default App
